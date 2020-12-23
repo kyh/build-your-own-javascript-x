477407da308b4ef7f02b50ce3d5627e171e5e4f8
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-12-24 06:22:15 +0800

    add comments, move to more functional style

--- styled-components/styled.js
+++ styled-components/styled.js
@@ -1,65 +1,102 @@
+// Styled Components uses Stylis for CSS preprocessing
 const { serialize, compile, stringify } = stylis;
+// Used to generate a unique ID per styled component
 let counter = 0;
+// Reference to the <style> tag injected in the <head>
 let styleTag = null;
 
+/**
+ * Simple hashing function, the real styled-components uses the MurmurHash
+ * algorithm and then converts the hash number to an alphabetic name:
+ *
+ * https://github.com/styled-components/styled-components/blob/v3.3.3/src/utils/generateAlphabeticName.js#L13
+ *
+ * @param {String} s
+ * @returns {Number}
+ */
 const hash = (s) =>
   s.split("").reduce((a, b) => {
     a = (a << 5) - a + b.charCodeAt(0);
     return a & a;
   }, 0);
 
+/**
+ * If the custom style tag doesn't exist, we create and inject it into
+ * the head
+ */
+const createOrGetStyleTag = () => {
+  if (!styleTag) {
+    const style = document.createElement("style");
+    style.setAttribute("data-styled-components", "");
+    document.head.appendChild(style);
+    styleTag = style;
+  }
+  return styleTag;
+};
+
+/**
+ * Concat all the string chunks from the tagged template[1] with the
+ * results of the expressions one by one; and if an expression is a
+ * function it is called with the component’s props.
+ */
+const interpolateStyles = (strs, exprs, props) => {
+  const evaluatedStyles = exprs.reduce((result, expr, index) => {
+    const isFunc = typeof expr === "function";
+    const value = isFunc ? expr(props) : expr;
+    return result + value + strs[index + 1];
+  }, strs[0]);
+
+  return evaluatedStyles;
+};
+
+/**
+ * Classnames are unique based off the componentId and evaluated styles.
+ * Which means if we evaluate to new styles we create a new classname. Unused
+ * styles aren't removed because of unecessary performance overhead:
+ *
+ * https://github.com/styled-components/styled-components/issues/1431#issuecomment-358097912
+ */
+const createClassname = (componentId, evaluatedStyles) => {
+  const className = "c-" + hash(componentId + evaluatedStyles);
+  return className;
+};
+
+/**
+ * Inject the styles into the <style> tag if the className doesn't already
+ * exist
+ */
+const injectStyles = (className, evaluatedStyles) => {
+  const selector = "." + className;
+  const style = createOrGetStyleTag();
+  if (!style.innerText.includes(selector)) {
+    const cssStr = serialize(
+      compile(`${selector}{${evaluatedStyles}}`),
+      stringify
+    );
+    style.appendChild(document.createTextNode(cssStr));
+  }
+};
+
 const styled = (TargetComponent) => (strs, ...exprs) => {
   counter++;
   const componentId = "sc-" + hash("sc" + counter);
+
   class StyledComponent extends React.Component {
     constructor(props) {
       super(props);
-      const { className, evaluatedStyles } = this.interpolateStyles();
-      this.injectStyles(className, evaluatedStyles);
+      const evaluatedStyles = interpolateStyles(strs, exprs, props);
+      const className = createClassname(componentId, evaluatedStyles);
+      injectStyles(className, evaluatedStyles);
       this.state = {
         generatedClassName: className,
       };
     }
 
-    interpolateStyles() {
-      const evaluatedStyles = exprs.reduce((result, expr, index) => {
-        const isFunc = typeof expr === "function";
-        const value = isFunc ? expr(this.props) : expr;
-
-        return result + value + strs[index + 1];
-      }, strs[0]);
-
-      const className = "c-" + hash(componentId + evaluatedStyles);
-
-      return { className, evaluatedStyles };
-    }
-
-    injectStyles(className, evaluatedStyles) {
-      const selector = "." + className;
-      const style = this.createOrGetStyleTag();
-      if (!style.innerText.includes(selector)) {
-        const cssStr = serialize(
-          compile(`${selector}{${evaluatedStyles}}`),
-          stringify
-        );
-        style.appendChild(document.createTextNode(cssStr));
-      }
-    }
-
-    createOrGetStyleTag() {
-      if (!styleTag) {
-        const head = document.head || document.getElementsByTagName("head")[0];
-        const style = document.createElement("style");
-        head.appendChild(style);
-        styleTag = style;
-      }
-      return styleTag;
-    }
-
     componentDidUpdate() {
-      const { className, evaluatedStyles } = this.interpolateStyles();
+      const evaluatedStyles = interpolateStyles(strs, exprs, this.props);
+      const className = createClassname(componentId, evaluatedStyles);
       if (className !== this.state.generatedClassName) {
-        this.injectStyles(className, evaluatedStyles);
+        injectStyles(className, evaluatedStyles);
         this.setState({
           generatedClassName: className,
         });
@@ -78,6 +115,8 @@ const styled = (TargetComponent) => (strs, ...exprs) => {
     }
   }
 
+  StyledComponent.componentId = componentId;
+
   return StyledComponent;
 };
 
@@ -85,3 +124,21 @@ styled.button = styled("button");
 styled.span = styled("span");
 
 window.styled = styled;
+
+/**
+[1]
+A tagged template:
+
+const Button = styled.button`
+  color: coral;
+  padding: 0.25rem 1rem;
+`;
+
+Is the same as:
+
+const Button = styled('button')([
+  'color: coral;' +
+  'padding: 0.25rem 1rem;'
+]);
+
+*/
