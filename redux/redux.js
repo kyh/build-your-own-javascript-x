fd894cfcce0cceae166cdb67da6989fb04009cde
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-12-24 11:02:07 +0800

    finish redux example

--- redux/redux.js
+++ redux/redux.js
@@ -1,19 +1,17 @@
 const createStore = (reducer, initialState) => {
-  const store = {};
-  store.state = initialState;
-  store.listeners = [];
-
-  store.getState = () => store.state;
-
-  store.subscribe = (listener) => {
-    store.listeners.push(listener);
+  return {
+    state: initialState,
+    listeners: [],
+    getState: () => {
+      return store.state;
+    },
+    subscribe: (listener) => {
+      store.listeners.push(listener);
+    },
+    dispatch: (action) => {
+      console.log("> Action", action);
+      store.state = reducer(store.state, action);
+      store.listeners.forEach((listener) => listener());
+    },
   };
-
-  store.dispatch = (action) => {
-    console.log("> Action", action);
-    store.state = reducer(store.state, action);
-    store.listeners.forEach((listener) => listener());
-  };
-
-  return store;
 };
