fe99595df73d5aeaa3c1624adcef663bfae26cb5
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-01-12 19:45:22 -0800

    simplify react library

--- react/react.js
+++ react/react.js
@@ -1,264 +1,171 @@
-/**
- * createElement creates and returns a new React element of a given type.
- *
- * @param {*} type - the type of the element we are creating. This can be
- *    either be a HTML element or a React component. If we are creating a
- *    HTML element, the name of the element (div, p etc.) is passed as a
- *    string. If we are creating a React component, the variable that the
- *    component is assigned to is passed as the value.
- * @param {*} props - An object containing the properties (props) that get
- *    passed to the component.
- * @param  {...any} children - The children of the component. You can pass
- *    as many children as you want.
- *
- * ```
- *  Example response:
-    {
-      type: 'div',
-      props: {
-        className: 'my-class',
-        randomProp: 'randomValue',
-        children: [{
-          type: 'button',
-          props: { className: 'blue' }
-        }, {
-          type: 'button',
-          props: { className: 'red' }
-        }]
-      },
+const createElement = (type, props, ...children) => {
+  if (props === null) props = {};
+  return { type, props, children };
+};
+
+const setAttribute = (dom, key, value) => {
+  if (typeof value == "function" && key.startsWith("on")) {
+    const eventType = key.slice(2).toLowerCase();
+    dom.__reactHandlers = dom.__reactHandlers || {};
+    dom.removeEventListener(eventType, dom.__reactHandlers[eventType]);
+    dom.__reactHandlers[eventType] = value;
+    dom.addEventListener(eventType, dom.__reactHandlers[eventType]);
+  } else if (key == "checked" || key == "value" || key == "className") {
+    dom[key] = value;
+  } else if (key == "style" && typeof value == "object") {
+    Object.assign(dom.style, value);
+  } else if (key == "ref" && typeof value == "function") {
+    value(dom);
+  } else if (key == "key") {
+    dom.__reactKey = value;
+  } else if (typeof value != "object" && typeof value != "function") {
+    dom.setAttribute(key, value);
+  }
+};
+
+const render = (vdom, parent = null) => {
+  const mount = parent ? (el) => parent.appendChild(el) : (el) => el;
+  if (typeof vdom == "string" || typeof vdom == "number") {
+    return mount(document.createTextNode(vdom));
+  } else if (typeof vdom == "boolean" || vdom === null) {
+    return mount(document.createTextNode(""));
+  } else if (typeof vdom == "object" && typeof vdom.type == "function") {
+    return Component.render(vdom, parent);
+  } else if (typeof vdom == "object" && typeof vdom.type == "string") {
+    const dom = mount(document.createElement(vdom.type));
+    for (const child of [].concat(...vdom.children)) render(child, dom);
+    for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
+    return dom;
+  } else {
+    throw new Error(`Invalid VDOM: ${vdom}.`);
+  }
+};
+
+const patch = (dom, vdom, parent = dom.parentNode) => {
+  const replace = parent
+    ? (el) => parent.replaceChild(el, dom) && el
+    : (el) => el;
+  if (typeof vdom == "object" && typeof vdom.type == "function") {
+    return Component.patch(dom, vdom, parent);
+  } else if (typeof vdom != "object" && dom instanceof Text) {
+    return dom.textContent != vdom ? replace(render(vdom, parent)) : dom;
+  } else if (typeof vdom == "object" && dom instanceof Text) {
+    return replace(render(vdom, parent));
+  } else if (
+    typeof vdom == "object" &&
+    dom.nodeName != vdom.type.toUpperCase()
+  ) {
+    return replace(render(vdom, parent));
+  } else if (
+    typeof vdom == "object" &&
+    dom.nodeName == vdom.type.toUpperCase()
+  ) {
+    const pool = {};
+    const active = document.activeElement;
+    [].concat(...dom.childNodes).map((child, index) => {
+      const key = child.__reactKey || `__index_${index}`;
+      pool[key] = child;
+    });
+    [].concat(...vdom.children).map((child, index) => {
+      const key = (child.props && child.props.key) || `__index_${index}`;
+      dom.appendChild(pool[key] ? patch(pool[key], child) : render(child, dom));
+      delete pool[key];
+    });
+    for (const key in pool) {
+      const instance = pool[key].__reactInstance;
+      if (instance) instance.componentWillUnmount();
+      pool[key].remove();
     }
- * ```
- */
-const createElement = (type, props, ...children) => ({
-  type,
-  props: {
-    ...props,
-    children: children.flat(1),
-  },
-});
+    for (const attr of dom.attributes) dom.removeAttribute(attr.name);
+    for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
+    active.focus();
+    return dom;
+  }
+};
 
 class Component {
   constructor(props) {
-    this.props = props;
+    this.props = props || {};
+    this.state = null;
   }
 
-  setState(state) {
-    // Do not rerender if setState is called with null or undefined
-    if (state == null) {
-      return;
-    }
-
-    if (typeof state === "function") {
-      this.state = { ...this.state, ...state(this.state) };
+  static render(vdom, parent = null) {
+    const props = Object.assign({}, vdom.props, { children: vdom.children });
+    if (Component.isPrototypeOf(vdom.type)) {
+      const instance = new vdom.type(props);
+      instance.componentWillMount();
+      instance.base = render(instance.render(), parent);
+      instance.base.__reactInstance = instance;
+      instance.base.__reactKey = vdom.props.key;
+      instance.componentDidMount();
+      return instance.base;
     } else {
-      this.state = { ...this.state, ...state };
+      return render(vdom.type(props), parent);
     }
-
-    setTimeout(render, 0);
   }
 
-  render() {
-    throw new Error(
-      "React.Component may not be used directly. Create your own class which extends this class."
-    );
+  static patch(dom, vdom, parent = dom.parentNode) {
+    const props = Object.assign({}, vdom.props, { children: vdom.children });
+    if (dom.__reactInstance && dom.__reactInstance.constructor == vdom.type) {
+      dom.__reactInstance.componentWillReceiveProps(props);
+      dom.__reactInstance.props = props;
+      return patch(dom, dom.__reactInstance.render(), parent);
+    } else if (Component.isPrototypeOf(vdom.type)) {
+      const ndom = Component.render(vdom, parent);
+      return parent ? parent.replaceChild(ndom, dom) && ndom : ndom;
+    } else if (!Component.isPrototypeOf(vdom.type)) {
+      return patch(dom, vdom.type(props), parent);
+    }
+  }
+
+  setState(next) {
+    const compat = (a) => typeof this.state == "object" && typeof a == "object";
+    if (this.base && this.shouldComponentUpdate(this.props, next)) {
+      const prevState = this.state;
+      this.componentWillUpdate(this.props, next);
+      this.state = compat(next) ? Object.assign({}, this.state, next) : next;
+      patch(this.base, this.render());
+      this.componentDidUpdate(this.props, prevState);
+    } else {
+      this.state = compat(next) ? Object.assign({}, this.state, next) : next;
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps != this.props || nextState != this.state;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    return undefined;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    return undefined;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    return undefined;
+  }
+
+  componentWillMount() {
+    return undefined;
+  }
+
+  componentDidMount() {
+    return undefined;
+  }
+
+  componentWillUnmount() {
+    return undefined;
   }
 }
 
 Component.prototype.isReactComponent = true;
 
-/**
- * `VDomNode` is a "virtual" DOM-node. Everything that can be represented
- * in the DOM, such as a number, string, div, a, p etc. should be a VDomNode
- */
-class VDomNode {
-  static isEmpty(reactElement) {
-    return reactElement === undefined || reactElement == null;
-  }
-
-  static isPrimitive(reactElement) {
-    return (
-      !reactElement.type &&
-      (typeof reactElement === "string" || typeof reactElement === "number")
-    );
-  }
-
-  static getChildrenAsArray(props) {
-    const { children = [] } = props || {};
-    return !Array.isArray(children) ? [children] : children;
-  }
-
-  static setAttributes(domNode, props = {}) {
-    const { className, style, ...restProps } = props;
-
-    // Set className
-    if (className) {
-      domNode.className = className;
-    }
-
-    // Set styles
-    if (style) {
-      Object.entries(style).forEach(([key, value]) => {
-        domNode.style[key] = value;
-      });
-    }
-
-    // Add event listeners and other props
-    Object.entries(restProps).forEach(([key, value]) => {
-      if (key === "children") {
-        return;
-      }
-
-      if (/^on.*$/.test(key)) {
-        domNode.addEventListener(key.substring(2).toLowerCase(), value);
-      } else if (key === "value") {
-        domNode.value = value;
-      } else {
-        domNode.setAttribute(key, value);
-      }
-    });
-  }
-
-  static buildDomNode(reactElement) {
-    if (VDomNode.isEmpty(reactElement)) {
-      return document.createTextNode(""); // Empty node
-    }
-
-    if (VDomNode.isPrimitive(reactElement)) {
-      return document.createTextNode(reactElement);
-    }
-
-    const { type, props } = reactElement;
-
-    const domNode = document.createElement(type);
-    VDomNode.setAttributes(domNode, props);
-
-    return domNode;
-  }
-
-  constructor(reactElement) {
-    this.currentReactElement = reactElement;
-    this.domNode = null;
-  }
-
-  getPublicInstance() {
-    return this.domNode;
-  }
-
-  mount(classCache) {
-    const { props } = this.currentReactElement || {};
-
-    this.domNode = VDomNode.buildDomNode(this.currentReactElement);
-
-    const childrenVNodes = VDomNode.getChildrenAsArray(props).map(
-      instantiateVNode
-    );
-
-    for (const childVNode of childrenVNodes) {
-      const childDomNode = childVNode.mount(classCache);
-      this.domNode.appendChild(childDomNode);
-    }
-
-    return this.domNode;
-  }
-}
-
-/**
- * VCompositeNode` represents a "virtual" react-component node. Everything
- * else, and by that we mean stateful - or stateless-components should be a
- * VCompositeNode.
- */
-class VCompositeNode {
-  static isReactClassComponent(type) {
-    return type.prototype && type.prototype.isReactComponent;
-  }
-
-  static isVCompositeNode(type) {
-    return typeof type === "function";
-  }
-
-  constructor(reactElement) {
-    this.currentReactElement = reactElement;
-    this.classInstance = null;
-  }
-
-  getPublicInstance() {
-    return this.classInstance;
-  }
-
-  mount(classCache) {
-    const { type, props } = this.currentReactElement;
-
-    let renderedInstance;
-    if (VCompositeNode.isReactClassComponent(type)) {
-      const cacheIndex = classCache.index++;
-      const cachedInstance = classCache.cache[cacheIndex];
-
-      const instance = cachedInstance ? cachedInstance : new type(props);
-      instance.props = props;
-
-      classCache.cache[cacheIndex] = instance;
-
-      renderedInstance = instantiateVNode(instance.render());
-      this.classInstance = instance;
-    } else {
-      renderedInstance = instantiateVNode(type(props));
-      this.classInstance = null;
-    }
-
-    return renderedInstance.mount(classCache);
-  }
-}
-
-/**
- * With the class-cache we can just replace every node on a render of the DOM
- * and just pop stateful components from the cache whenever we encounter one.
- * That way, we won't loose that state of the component when re-rendering.
- *
- * This is instead of having to traverse the virtual-DOM tree and compare
- * every child-node with it's previous instance and then removing, replacing
- * or appending that child to the node.
- */
-const classCache = {
-  index: -1,
-  cache: [],
-};
-
-const instantiateVNode = (reactElement) => {
-  const { type } = reactElement || {};
-
-  if (VCompositeNode.isVCompositeNode(type)) {
-    return new VCompositeNode(reactElement);
-  }
-
-  return new VDomNode(reactElement);
-};
-
-const root = {};
-
-const render = (
-  reactElement = root.reactElement,
-  domContainerNode = root.domContainerNode
-) => {
-  if (root.domContainerNode) {
-    domContainerNode.innerHTML = "";
-    classCache.index = -1;
-  }
-
-  const vNode = instantiateVNode(reactElement);
-  const domNode = vNode.mount(classCache);
-
-  domContainerNode.appendChild(domNode);
-
-  root.reactElement = reactElement;
-  root.domContainerNode = domContainerNode;
-
-  return vNode.getPublicInstance();
-};
-
 const React = {
+  render,
   createElement,
   Component,
-  render,
 };
 
 window.React = React;
